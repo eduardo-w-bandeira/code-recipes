d5e8af638dbf71ba580858ec5140fa3d8816c9de
Eduardo Willians Bandeira <78479953+eduardo-willians@users.noreply.github.com>
2022-12-17 08:24:48 -0400

    Choose app to run from a dir .py list

--- run-app.py
+++ run-app.py
@@ -1,16 +1,50 @@
-from commons import trails
+from pathlib import Path
+
+
+APPS_PATH = Path('my-dir-path')
+
+
+def latest_modified_file(paths):
+    mtimes_n_paths = []
+    for path in paths:
+        mtime = path.stat().st_mtime
+        mtimes_n_paths.append((mtime, path))
+    latter = sorted(mtimes_n_paths)[-1]
+    return latter[1]
+
+
+def path_to_smodule(path):
+    module = path.name[:-3]
+    appsdir = path.parent.parent.name
+    appdir = path.parent.name
+    return f'{appsdir}.{appdir}.{module}'
+
 
 def main():
-    filenames = trails.filter_filenames('apps', '', 'py')
-    app_names = [i[:i.find('.py')] for i in filenames]
-    for option, name in enumerate(app_names, start=1):
-        print(f'[{option}] {name}')
+    option = 1
+    option_path_map = {}
+    for item in sorted(conf.APPS.iterdir()):
+        if item.is_dir():
+            # print(item.name)
+            for subitem in sorted(item.iterdir()):
+                if subitem.is_file() and subitem.suffix == '.py':
+                    print(f'{option}: {subitem.name}')
+                    option_path_map[option] = subitem
+                    option += 1
+    print()
+    latest = latest_modified_file(option_path_map.values())
+    print(f'l: {latest} (latest modified file)')
     print()
     choice = input('Choose: ')
-    if not choice.isdigit():
+    if choice == 'l':
+        path = latest
+    elif choice.isdigit():
+        path = option_path_map[int(choice)]
+    else:
         return
-    app_name = app_names[int(choice)-1]
-    app = __import__(f'apps.{app_name}', fromlist=['apps'])
+    smodule = path_to_smodule(path)
+    app = __import__(smodule, fromlist=[APPS.name])
     app.main()
+
 
 main()
